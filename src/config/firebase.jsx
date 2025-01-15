033ec6cc13a6777160aacba1c338267e8c788153
Henabakos <henogato9876@gmail.com>
2025-01-15 21:19:05 +0300

    added responsiveness to all pages

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -18,7 +18,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig);
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
